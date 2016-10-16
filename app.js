d3f293f92d5b85dde179b0bb635322cf7785d03c
aashil <aashilpatel1993@gmail.com>
2016-10-16 17:37:30 -0400

    Add descriptive comments in the backend.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var obj = JSON.parse(fs.readFileSync('data/channel.json', 'utf8'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//Set the port number
 var port = process.env.PORT || 3000;
 var router = express.Router();
 
@@ -17,10 +18,11 @@ app.use(function(req, res, next) {
   next();
 });
 
+//Create a route for sending JSON data
 router.get('/data', function (req, res){
   res.json(obj);
 });
 
 app.use('/', router);
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Backend running on port: ' + port);
