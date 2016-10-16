f31d3b73b9ddbeb43bf21d13a6bbf2e10c022a6d
aashil <aashilpatel1993@gmail.com>
2016-10-16 01:22:58 -0400

    Add app.component.css file which will contain the style code related to app.component.html

--- app/app.component.js
+++ app/app.component.js
@@ -19,7 +19,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            templateUrl: 'app/app.component.html'
+            templateUrl: 'app/app.component.html',
+            styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [app_service_1.AppService])
     ], AppComponent);
