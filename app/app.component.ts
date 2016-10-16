7eb03012c4a34df744ebcc299f2fbe0ae7f9321f
aashil <aashilpatel1993@gmail.com>
2016-10-16 12:34:14 -0400

    Create a method to display the time of the course.
    * Make the data variable in app.component private.

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,13 +8,26 @@ import { AppService } from './app.service';
 })
 
 export class AppComponent {
-    data: any;
+    private data: any;
 
     constructor(private appService: AppService){
         this.appService.getChannels().subscribe(
             resp => this.data = resp,
             error => console.log(error)
         );
+    }
+
+    private displayTime(dateString: string){
+        let date = new Date(dateString);
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        let padMinutes = minutes < 10 ? '0'+minutes : minutes;
+        let strTime = hours + ':' + padMinutes + ' ' + ampm + ' - ' +
+            (hours+1) + ':' + padMinutes + ' ' + ampm + ' EDT';
+        return  strTime;
     }
 
 }
